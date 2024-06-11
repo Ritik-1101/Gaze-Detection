25ac5d985df513409dc832cde48cbb149e89efa9
Ritik-1101 <drakerespawn@gmail.com>
2024-06-11 19:56:25 -0400

    ssShape

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ def process_image():
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000, ssl_context=('server.crt', 'server.key'))
